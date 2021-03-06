e821ae4a9cc94bb78986cc7b417a7dbbbf4f3c0e
tychien <r07525118@ntu.edu.tw>
2021-03-06 23:33:43 +0800

    color

--- color.py
+++ color.py
@@ -42,30 +42,7 @@ while (True):
         cv2.destroyAllWindows()
         break
 
-'''
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (9,9),0)
-    edged = cv2.Canny(gray, 50, 150)
-    edged = cv2.dilate(edged, None, iterations=3)
-    contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-    print('Num. of Contours = '+ str(len(contours)))
-    print('Contours[0]:'+str(len(contours[0])))
-
-    for n in contours:
-        approx = cv2.approxPolyDP(n, 5, True)
-        cv2.drawContours(frame, [approx], -1, (255,0,0),5)
-        if len(approx) == 4:
-            cv2.drawContours(frame, [approx], -1, (0,0,255) , 5)
-    
-    
-    
-    cv2.imshow('contour',frame)
-    cv2.imshow('frame', edged)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-'''
 
 cap.release()
 
